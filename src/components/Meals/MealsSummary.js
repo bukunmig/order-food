a39d99b15ffd3f54c65f7e4327253f7c677c65fe
bukunmig <bgesinde44@icloud.com>
2023-02-05 22:35:56 -0800

    Updated menu form

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -5,8 +5,9 @@ const MealsSummary = () => {
     <section className={classes.summary}>
       <h2>Delicious Food, Delivered To You</h2>
       <p>
+        Looking to make a catering order with an up and coming chef in your area?
         Support Local upcoming talent in your neighborhood. Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        and enjoy a delicious lunch or dinner at home. Providing a platform for emerging talent...
       </p>
       <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
