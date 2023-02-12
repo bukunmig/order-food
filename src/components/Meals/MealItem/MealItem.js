b405400c98ea3a74c0543b7365502b853a9679e8
bukunmig <bgesinde44@icloud.com>
2023-02-11 19:15:52 -0800

    Updated cart validation

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,22 +1,22 @@
-// // import { useContext } from 'react';
-
-// // import MealItemForm from './MealItemForm';
+import { useContext } from 'react';
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
-// // import CartContext from '../../../store/cart-context';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
-// //   const cartCtx = useContext(CartContext);
+
+  const cartCtx = useContext(CartContext);
 
   const price = `$${props.price.toFixed(2)}`;
 
-// //   const addToCartHandler = amount => {
-// //     cartCtx.addItem({
-// //       id: props.id,
-// //       name: props.name,
-// //       amount: amount,
-// //       price: props.price
-// //     });
-// //   };
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -25,9 +25,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      {/* <div>
-        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
-      </div> */}
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
+      </div> 
     </li>
   );
 };
